1dde3481805e2badfd3d1cd943ce4b7b1db5d63a
Cuza <baopham92@yahoo.com>
2018-09-23 18:20:56 -0400

    Added list for contacts. Logic for methods added.

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,13 +1,14 @@
 class AddressBook {
     constructor() {
-
-    }
-
-    addContact() {
-
-    }
-
-    checkContact() {
         
+        this.contactList = [];
+    }
+
+    addContact(contact) {
+        return this.contactList.push(contact);
+    }
+
+    checkContact(index) {
+        return this.contactList[index];
     }
 }
