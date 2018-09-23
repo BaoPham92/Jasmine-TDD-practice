04a766e0c72643203c05cd5d2c2e863eb283bbb0
Cuza <baopham92@yahoo.com>
2018-09-23 18:01:11 -0400

    Added expectations and addContact functionality.

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -2,6 +2,8 @@ describe('Address book', function(){
     it('This should be able to add a contact.', function() {
         let addressBook = new addressBook(), newContact = new newContact();
 
-        
+        addressBook.addContact(newContact);
+
+        expect(addressBook.checkContact(0)).toBe(newContact);
     })
 })
