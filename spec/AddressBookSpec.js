e6d82ebf4fef9d2ee0760f5df152b7354cb9563e
Cuza <baopham92@yahoo.com>
2018-09-23 18:28:01 -0400

    Created second spec for deleting a contact.

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,9 +1,19 @@
-describe('Address book', function(){
+describe('Address book', function() {
+
     it('This should be able to add a contact.', function() {
         let addressBook = new AddressBook(), newContact = new NewContact();
 
         addressBook.addContact(newContact);
 
         expect(addressBook.checkContact(0)).toBe(newContact);
+    })
+
+    it('This should be able to delete a contact.', function() {
+        let addressBook = new AddressBook(), newContact = new NewContact();
+
+        addressBook.addContact(newContact);
+        addressBook.deleteContact(0);
+
+        expect(addressBook.checkContact(0)).not.toBeDefined();
     })
 })
